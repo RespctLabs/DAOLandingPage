5c41733b10348bad6d12be7e9aa16df9607193f0
Sandeep <itissandeep98@gmail.com>
2022-06-05 12:55:52 +0530

    code cleanup

--- layouts/LayoutWrapper.jsx
+++ layouts/LayoutWrapper.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import Navbar from '../components/navbar';
-import Meta from './Meta';
+import React from "react";
+import Meta from "./Meta";
 
 const LayoutWrapper = (params) => {
-  const { children, ...props } = params;
-  return (
-    <div className="w-11/12 mx-auto text-white">
-      <Meta />
-      <Navbar />
-      {React.cloneElement(children, { ...props })}
-    </div>
-  );
+	const { children, ...props } = params;
+	return (
+		<div className="w-11/12 mx-auto text-white">
+			<Meta />
+			{React.cloneElement(children, { ...props })}
+		</div>
+	);
 };
 
 export default LayoutWrapper;
