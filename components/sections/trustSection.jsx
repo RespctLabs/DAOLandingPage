9a57c60bac254404f3ef7abdbadde4cb0e1a35e0
Sandeep <itissandeep98@gmail.com>
2022-06-06 18:10:21 +0530

    minor fix

--- components/sections/trustSection.jsx
+++ components/sections/trustSection.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 
-
 function trustSection() {
 	return (
 		<>
 			<div id="trustBody">
 				<div
 					id="moveLeft"
-					style={{ width: "70%", marginTop: "5%",display: "flex" }}
+					style={{ width: "70%", marginTop: "5%", display: "flex" }}
 				>
-					<img src={"/images/panel.png"} style={{ marginRight: "3%" }}/>
-					<img src={"/images/panel.png"} style={{ marginRight: "3%" }}/>
-					<img src={"/images/panel.png"} style={{ marginRight: "3%" }}/>
-					<img src={"/images/panel.png"} style={{ marginRight: "3%" }}/>
-					<img src={"/images/panel.png"} style={{ marginRight: "3%" }}/>
-					
+					<img src={"/images/panel.png"} style={{ marginRight: "3%" }} />
+					<img src={"/images/panel.png"} style={{ marginRight: "3%" }} />
+					<img src={"/images/panel.png"} style={{ marginRight: "3%" }} />
+					<img src={"/images/panel.png"} style={{ marginRight: "3%" }} />
+					<img src={"/images/panel.png"} style={{ marginRight: "3%" }} />
 				</div>
-				<div style={{ display: "flex" }} className="font-bold pb-4 text-center text-7xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-800">
+				<div
+					style={{ display: "flex" }}
+					className="font-bold pb-4 text-center text-7xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-800"
+				>
 					<span id="titleColor" style={{ marginRight: "0.3em" }}>
 						Trusted
 					</span>
-					
 				</div>
 				<img
 					src={"/images/Games.png"}
-					style={{ position: "absolute", right: "2vw", top: "5vw", opacity:"0.8" }}
+					style={{
+						position: "absolute",
+						right: "2vw",
+						top: "5vw",
+						opacity: "0.8",
+					}}
 				></img>
 				<img
 					src={"/images/Guilds.png"}
@@ -33,7 +38,7 @@ function trustSection() {
 						left: "2vw",
 						bottom: "5vw",
 						zIndex: "10",
-						opacity: "0.8"
+						opacity: "0.8",
 					}}
 				></img>
 				<p id="descp" style={{ fontSize: "1.2rem" }}>
@@ -43,20 +48,21 @@ function trustSection() {
 					Let&apos;s build the metaverse by empowering people to be a part of
 					it.
 				</p>
-				<button className="px-4 py-2 bg-gradient-to-r from-cyan-500 to-violet-800 rounded-2xl hover:from-violet-800 hover:to-cyan-500 transform duration-200 ease-in" id="ebt">
-				Early Access
-			</button>
+				<button
+					className="px-4 py-2 bg-gradient-to-r from-cyan-500 to-violet-800 rounded-2xl hover:from-violet-800 hover:to-cyan-500 transform duration-200 ease-in"
+					id="ebt"
+				>
+					Early Access
+				</button>
 				<img
 					id="moveRight"
-					style={{ width: "70%", marginBottom: "5%",display: "flex"}}
-				>
-					<img src={"/images/panel.png"} style={{ marginRight: "3%" }}/>
-					<img src={"/images/panel.png"} style={{ marginRight: "3%" }}/>
-					<img src={"/images/panel.png"} style={{ marginRight: "3%" }}/>
-					<img src={"/images/panel.png"} style={{ marginRight: "3%" }}/>
-					<img src={"/images/panel.png"} style={{ marginRight: "3%" }}/>
-					
-				</div>
+					style={{ width: "70%", marginBottom: "5%", display: "flex" }}
+				/>
+				<img src={"/images/panel.png"} style={{ marginRight: "3%" }} />
+				<img src={"/images/panel.png"} style={{ marginRight: "3%" }} />
+				<img src={"/images/panel.png"} style={{ marginRight: "3%" }} />
+				<img src={"/images/panel.png"} style={{ marginRight: "3%" }} />
+				<img src={"/images/panel.png"} style={{ marginRight: "3%" }} />
 			</div>
 		</>
 	);
