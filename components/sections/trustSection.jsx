ccc1283b7f41796546f2d7e0b49aebaf3329b3db
Abhinav Rana <abhinavrana@Abhinavs-MacBook-Pro.local>
2022-06-06 16:42:23 +0530

    third comm

--- components/sections/trustSection.jsx
+++ components/sections/trustSection.jsx
@@ -9,15 +9,9 @@ function trustSection() {
 					src={"/images/panel.png"}
 					style={{ width: "70%", marginTop: "5%" }}
 				/>
-<<<<<<< HEAD
 				<div style={{ display: "flex" }}>
 					<span id="titleColor" style={{ marginRight: "0.3em", fontSize: "1.2rem" }}>
 						Trusted by the industry
-=======
-				<div style={{ display: "flex" }} className="font-bold pb-4 text-center text-7xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-800">
-					<span id="titleColor" style={{ marginRight: "0.3em" }}>
-						Trusted
->>>>>>> 1cd01c1a78dc577d7f8a28131b867d5e31732704
 					</span>
 					
 				</div>
@@ -42,11 +36,7 @@ function trustSection() {
 					Let&apos;s build the metaverse by empowering people to be a part of
 					it.
 				</p>
-<<<<<<< HEAD
 				<div id="btn" style={{fontSize: "1.2rem"}}>Early Access</div>
-=======
-				<div className="px-4 py-2 bg-gradient-to-r from-cyan-500 to-violet-800 rounded-2xl hover:from-violet-800 hover:to-cyan-500 transform duration-200 ease-in">Early Access</div>
->>>>>>> 1cd01c1a78dc577d7f8a28131b867d5e31732704
 				<img
 					id="moveRight"
 					src={"/images/panel.png"}
